8dbf91b2d231b568d34f9d97652c881437da646d
My Pham <mypham@mys-macbook-pro.lan>
2023-08-15 07:54:01 -0700

    add csv file

--- hackerNewsScraping.py
+++ hackerNewsScraping.py
@@ -6,16 +6,17 @@ soup = BeautifulSoup(res.text, "html.parser")
 elements = soup.select(".titleline > a")
 print(elements)
 
+fileName = 'hackerNewsData.csv'
+f = open(fileName, "w")
+headers = "Title, Link, Upvotes \n"
+f.write(headers)
+
 def get_data(elements):
-    hn = []
     for element in elements:
         title = element.getText()
         href = element.get("href")
         upvote = element.find_next(class_="score")
         points = upvote.getText()
-
-        hn.append({"title": title, "href": href, "points": points})
-    return hn
-
-
-print(get_data(elements))
+        f.write(title.replace(",", "|") + "," + href + "," + points + "\n")
+get_data(elements)
+f.close()
